767f4f8e990abaaedfaa9a1348c460de31afacd8
yuki1229h10 <yuki1229.h10@i.softbank.jp>
2024-07-16 23:51:41 +0900

    add

--- src/main/java/com/example/spring_boot_blog_application/controller/RegisterController.java
+++ src/main/java/com/example/spring_boot_blog_application/controller/RegisterController.java
@@ -1,13 +1,29 @@
 package com.example.spring_boot_blog_application.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.spring_boot_blog_application.models.Account;
+import com.example.spring_boot_blog_application.services.AccountService;
 
 @Controller
 public class RegisterController {
 
+	@Autowired
+	private AccountService accountService;
+
 	@GetMapping("/register")
 	public String getRegisterPage() {
 		return "register";
+	}
+
+	@PostMapping("/register")
+	public String registerNewUser(@ModelAttribute Account account) {
+		accountService.save(account);
+
+		return "redirect:/";
 	}
 }
